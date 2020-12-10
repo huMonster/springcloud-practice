5c5646c7308817a50d845d12096536e2d176380a
monster <39299084+huMonster@users.noreply.github.com>
2020-12-10 11:23:33 +0800

    Add files via upload

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/xtt/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.xtt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Description HystrixDashboardMain9001
+ * @Author Monster
+ * @Date 2020/12/10 9:37
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
